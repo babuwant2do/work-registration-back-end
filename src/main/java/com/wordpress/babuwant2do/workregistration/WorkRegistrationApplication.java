848823a18926c4e7de9840e65940662989f47222
Mohammed Ali <mohammedali@Mohammeds-MacBook-Pro.local>
2018-12-24 19:46:40 +0100

    init project just the starter with JPA, Security, Mysql, WEB

---
+++ src/main/java/com/wordpress/babuwant2do/workregistration/WorkRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.wordpress.babuwant2do.workregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkRegistrationApplication.class, args);
+	}
+
+}
+
